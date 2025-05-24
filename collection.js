aed7fc04e27977518da900a3225c08e5dccbd29a
Muhammad Mustafa Hussain <140386879+muhammadmustafarooqi@users.noreply.github.com>
2025-05-24 16:24:46 +0500

    Add files via upload

---
+++ collection.js
@@ -0,0 +1,24 @@
+const collection = [
+  { title: "Shadow Profile", img: "./assets/img1.jpg" },
+  { title: "Crimson Silhouette", img: "./assets/img2.jpg" },
+  { title: "Wavelength", img: "./assets/img3.jpg" },
+  { title: "Noir Figure", img: "./assets/img4.jpg" },
+  { title: "Midnight Gaze", img: "./assets/img5.jpg" },
+  { title: "Cobalt Contrast", img: "./assets/img6.jpg" },
+  { title: "Half-Light", img: "./assets/img7.jpg" },
+  { title: "Scarlet Frame", img: "./assets/img8.jpg" },
+  { title: "Pale Vision", img: "./assets/img9.jpg" },
+  { title: "Spectral Form", img: "./assets/img10.jpg" },
+  { title: "Monochrome Motion", img: "./assets/img11.jpg" },
+  { title: "Platinum Edge", img: "./assets/img12.jpg" },
+  { title: "Electric Shade", img: "./assets/img13.jpg" },
+  { title: "Veiled Light", img: "./assets/img14.jpg" },
+  { title: "Luminous Dark", img: "./assets/img15.jpg" },
+  { title: "Haze Portrait", img: "./assets/img16.jpg" },
+  { title: "Glowing Contour", img: "./assets/img17.jpg" },
+  { title: "Dark Elegance", img: "./assets/img18.jpg" },
+  { title: "Ruby Accent", img: "./assets/img19.jpg" },
+  { title: "Clear Gaze", img: "./assets/img20.jpg" }
+];
+
+export default collection;
